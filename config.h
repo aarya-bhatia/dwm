5c5592d669115db28aa71d8f8083eb56ff044cbc
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-06-18 17:03:11 -0700

    add rule

--- config.h
+++ config.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",        NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",     NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Chromium",    NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "thunderbird", NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "email",       NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "Ario",        NULL,       NULL,       1 << 3,       0,           -1 },
